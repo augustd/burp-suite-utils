6a2d7f7e38b92510318d84a1dc03d206bffb3c5e
August Detlefsen <august@Augusts-MacBook-Pro.local>
2015-09-14 00:57:47 -0700

    Added ScanIssueConfidence as a member

--- src/com/codemagi/burp/MatchRule.java
+++ src/com/codemagi/burp/MatchRule.java
@@ -12,7 +12,7 @@ public class MatchRule {
     private Integer matchGroup;
     private String type;
     private ScanIssueSeverity severity;
-
+    private ScanIssueConfidence confidence;
 
     public MatchRule(Pattern pattern, Integer matchGroup, String type) {
 	this.pattern = pattern;
@@ -25,6 +25,14 @@ public class MatchRule {
 	this.matchGroup = matchGroup;
 	this.type = type;
 	this.severity = severity;
+    }
+
+    public MatchRule(Pattern pattern, Integer matchGroup, String type, ScanIssueSeverity severity, ScanIssueConfidence confidence) {
+	this.pattern = pattern;
+	this.matchGroup = matchGroup;
+	this.type = type;
+	this.severity = severity;
+	this.confidence = confidence;
     }
 
     public MatchRule(String test, Pattern pattern, Integer matchGroup, String type) {
@@ -60,6 +68,10 @@ public class MatchRule {
 
     public ScanIssueSeverity getSeverity() {
 	return severity;
+    }
+
+    public ScanIssueConfidence getConfidence() {
+	return confidence;
     }
     
 }
