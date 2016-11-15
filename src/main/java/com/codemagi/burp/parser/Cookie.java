6c3beee741808c1070727c5ea6cfb845fafef697
adetlefsen-rms <augustd@codemagi.com>
2016-11-15 13:06:54 -0800

    Implemented ICookie
    Workaround for an issue in Burp Extender API. See:
    https://support.portswigger.net/customer/portal/questions/16742489-updat
    ecookiejar-domain-cannot-be-null

--- src/main/java/com/codemagi/burp/parser/Cookie.java
+++ src/main/java/com/codemagi/burp/parser/Cookie.java
@@ -1,19 +1,34 @@
 package com.codemagi.burp.parser;
 
+import burp.ICookie;
+import java.util.Date;
+
 /**
  *
  * @author August Detlefsen [augustd at codemagi dot com]
  */
-public class Cookie {
+public class Cookie implements ICookie {
     
     String name;
     String value; 
+    String domain;
+    String path;
+    Date expiration;
 
     public Cookie(String name, String value) {
         this.name = name;
         this.value = value;
     }
+    
+    public Cookie(ICookie cookie) {
+        this.name = cookie.getName();
+        this.value = cookie.getValue();
+        this.domain = cookie.getDomain();
+        this.path = cookie.getPath();
+        this.expiration = cookie.getExpiration();
+    }
 
+    @Override
     public String getName() {
         return name;
     }
@@ -22,6 +37,7 @@ public class Cookie {
         this.name = name;
     }
 
+    @Override
     public String getValue() {
         return value;
     }
@@ -29,5 +45,32 @@ public class Cookie {
     public void setValue(String value) {
         this.value = value;
     }
+
+    @Override
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    @Override
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public Date getExpiration() {
+        return expiration;
+    }
     
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
 }
