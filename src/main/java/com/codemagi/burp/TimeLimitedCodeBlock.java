3cda4f4a957820606838880d6a1bf4cb229a788a
adetlefsen-rms <augustd@codemagi.com>
2017-06-26 16:34:26 -0700

    Util class for time limited actions

---
+++ src/main/java/com/codemagi/burp/TimeLimitedCodeBlock.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017 adetlefsen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codemagi.burp;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Taken From:
+ * http://stackoverflow.com/questions/5715235/java-set-timeout-on-a-certain-block-of-code
+ *
+ */
+public class TimeLimitedCodeBlock {
+
+    public static void runWithTimeout(final Runnable runnable, long timeout, TimeUnit timeUnit) throws Exception {
+        runWithTimeout(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                runnable.run();
+                return null;
+            }
+        }, timeout, timeUnit);
+    }
+
+    public static <T> T runWithTimeout(Callable<T> callable, long timeout, TimeUnit timeUnit) throws Exception {
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+        final Future<T> future = executor.submit(callable);
+        executor.shutdown(); // This does not cancel the already-scheduled task.
+        try {
+            return future.get(timeout, timeUnit);
+        } catch (TimeoutException e) {
+            //remove this if you do not want to cancel the job in progress
+            //or set the argument to 'false' if you do not want to interrupt the thread
+            future.cancel(true);
+            throw e;
+        } catch (ExecutionException e) {
+            //unwrap the root cause
+            Throwable t = e.getCause();
+            if (t instanceof Error) {
+                throw (Error) t;
+            } else if (t instanceof Exception) {
+                throw (Exception) e;
+            } else {
+                throw new IllegalStateException(t);
+            }
+        }
+    }
+
+}
