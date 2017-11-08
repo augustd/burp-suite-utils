df427a829600c4cbab1b06576f21a1888ef9d1a7
augustd <augustd@codemagi.com>
2017-11-08 15:25:21 -0800

    Fix loading rules from file
    Fixes https://github.com/augustd/burp-suite-error-message-checks/issues/26
    
    Also fixes ability to concurrently load match rules from remote URL when using UI buttons

--- src/main/java/burp/impl/HttpService.java
+++ src/main/java/burp/impl/HttpService.java
@@ -76,4 +76,8 @@ public class HttpService implements IHttpService {
         this.protocol = protocol;
     }
     
+    public boolean useHttps() {
+        return PROTOCOL_HTTPS.equalsIgnoreCase(protocol);
+    }
+    
 }
