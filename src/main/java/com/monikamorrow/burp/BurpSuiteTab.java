612e9f96d4fbe98443afbed61c17f848c00f4482
August <augustd@codemagi.com>
2019-03-21 08:56:43 -0700

    Formatting

--- src/main/java/com/monikamorrow/burp/BurpSuiteTab.java
+++ src/main/java/com/monikamorrow/burp/BurpSuiteTab.java
@@ -11,76 +11,78 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 /**
- * 
+ *
  * @author Monika Morrow Original URL: https://github.com/monikamorrow/Burp-Suite-Extension-Examples/tree/master/GUI%20Utils
  * @author August Detlefsen
  */
 public class BurpSuiteTab extends javax.swing.JPanel implements ITab {
+
     IBurpExtenderCallbacks mCallbacks;
-    String tabName; 
-    
+    String tabName;
+
     /**
      * Creates new form BurpSuiteTab
-     * @param tabName     The name displayed on the tab
-     * @param callbacks   For UI Look and Feel
+     *
+     * @param tabName The name displayed on the tab
+     * @param callbacks For UI Look and Feel
      */
     public BurpSuiteTab(String tabName, IBurpExtenderCallbacks callbacks) {
-	this.tabName = tabName;
+        this.tabName = tabName;
         mCallbacks = callbacks;
-        
+
         mCallbacks.customizeUiComponent(this);
         mCallbacks.addSuiteTab(this);
     }
-    
+
     public void addComponent(JPanel customPanel) {
         this.add(customPanel);
         mCallbacks.customizeUiComponent(customPanel);
         this.revalidate();
         this.doLayout();
     }
-    
+
     @Override
     public String getTabCaption() {
-	return tabName;
+        return tabName;
     }
 
     @Override
     public Component getUiComponent() {
-	return this;
+        return this;
     }
-	
-	/**
-	 * Highlights the tab using Burp's color scheme. The highlight disappears
-	 * after 3 seconds.
-	 */
-	public void highlight() {
-		final JTabbedPane parent = (JTabbedPane) this.getUiComponent().getParent();
 
-		//search through tabs until we find this one
-		for (int i = 0; i < parent.getTabCount(); i++) {
-			String title = parent.getTitleAt(i);
+    /**
+     * Highlights the tab using Burp's color scheme. The highlight disappears
+     * after 3 seconds.
+     */
+    public void highlight() {
+        final JTabbedPane parent = (JTabbedPane) this.getUiComponent().getParent();
 
-			if (getTabCaption().equals(title)) {  //found this tab
-				//create new colored label and set it into the tab
-				final JLabel label = new JLabel(getTabCaption());
-				label.setForeground(new Color(0xff6633));
-				parent.setTabComponentAt(i, label);
+        //search through tabs until we find this one
+        for (int i = 0; i < parent.getTabCount(); i++) {
+            String title = parent.getTitleAt(i);
 
-				//schedule a task to change back to original color
-				Timer timer = new Timer();
+            if (getTabCaption().equals(title)) {  //found this tab
+                //create new colored label and set it into the tab
+                final JLabel label = new JLabel(getTabCaption());
+                label.setForeground(new Color(0xff6633));
+                parent.setTabComponentAt(i, label);
 
-				TimerTask task = new TimerTask() {
-					@Override
-					public void run() {
-						label.setForeground(Color.black);
-					}
-				};
+                //schedule a task to change back to original color
+                Timer timer = new Timer();
 
-				timer.schedule(task, 3000);
+                TimerTask task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        label.setForeground(Color.black);
+                    }
+                };
 
-				break;
-			}
-		}
-	}
+                timer.schedule(task, 3000);
+
+                break;
+            }
+        }
+    }
 
 }
